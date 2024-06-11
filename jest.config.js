e45cc8adba143cc368dc206109a39de50fc7ee99
migbaranes <migbaranes@gmail.com>
2024-06-11 22:33:54 +0300

    first commit

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const config = {
+    collectCoverageFrom: [
+      '**/*.{js,jsx}',
+      '!**/node_modules/**',
+      '!**/coverage/**',
+    ],
+    "coverageThreshold": {
+        "branches": 100,
+        "functions": 100,
+        "lines": 100
+  },
+  };
+  
+module.exports = config;
